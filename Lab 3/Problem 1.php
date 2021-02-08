f743d5f1035078c1c92f0484054bc2902c96bb31
Jubaer Ahmad Nabil <jubaerahmadnabil@gmail.com>
2021-02-08 12:12:47 +0600

    Lab 3 created

---
+++ Lab 3/Problem 1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Problem 1 - Lab 3</title>
+</head>
+<body>
+
+	A quick brown <?php echo "<b>fox</b>"; ?> jumps over the lazy <?php echo "<b>dog</b>"; ?>
+
+</body>
+</html>
