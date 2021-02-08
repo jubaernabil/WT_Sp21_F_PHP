4611511f4d977cedaf7fadbdf5123acff2b154ee
Jubaer Ahmad Nabil <jubaerahmadnabil@gmail.com>
2021-02-08 12:54:02 +0600

    Lab task done

---
+++ Lab 3/Problem 3.php
@@ -0,0 +1,15 @@
+<?php
+		$a = 11;
+		$b = 44;
+		$Area = $a * $b ;
+		echo "Area = " . $Area . "<br/><br/>";
+		$Perimeter = 2*($a + $b);
+		echo "Perimeter = " . $Perimeter . "<br/><br/>";
+		if ($a==$b) 
+		{
+			echo "The shape is square!";
+		}
+		else{
+		      echo "The shape is not square!";	
+		}
+?>		
---
+++ Lab 3/Problem 4.php
@@ -0,0 +1,11 @@
+<?php
+		$name = 'Nabil';
+		if ($name=='Siam' or $name == 'Alvy' or $name == 'Nabil') 
+		{
+			echo "Hey " . $name . "<br/>";
+		}
+		else
+		{
+			echo "you are not recognised";
+		}
+	?>
